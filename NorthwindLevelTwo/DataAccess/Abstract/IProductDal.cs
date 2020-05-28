7a8fb1472f2e50b87b89761a7cae946275c8ec8a
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-28 12:51:25 +0300

    Getting started on Data Access layer

---
+++ NorthwindLevelTwo/DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        // Repository pattern
+        // Listing
+        // Adding
+        // Deleting
+        // Updating
+    }
+}
---
+++ NorthwindLevelTwo/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Abstract;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+
+    }
+}
