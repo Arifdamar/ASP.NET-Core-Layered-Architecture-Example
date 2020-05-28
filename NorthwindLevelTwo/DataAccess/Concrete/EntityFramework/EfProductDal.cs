1ffa7d7bd29526fb0fc15e5012f882ef27eb6501
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-28 15:35:34 +0300

    Implementing Repository

--- NorthwindLevelTwo/DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ NorthwindLevelTwo/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -2,36 +2,14 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using Core.DataAccess.Concrete.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public List<Product> GetList(Expression<Func<Product, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Add(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Product entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
