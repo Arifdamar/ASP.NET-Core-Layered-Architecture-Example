2f6d9f16cdd6b81d291c37628e58434c077418c6
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-29 13:12:38 +0300

    Getting started on Cart

---
+++ NorthwindLevelTwo/Business/Concrete/CartManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+using Entities.DomainModels;
+
+namespace Business.Concrete
+{
+    public class CartManager : ICartService
+    {
+        public void AddToCart(Cart cart, Product product)
+        {
+            CartLine cartLine = cart.CartLines.FirstOrDefault(line => line.Product.ProductId == product.ProductId);
+
+            if (cartLine != null)
+            {
+                cartLine.Quantity++;
+            }
+            else
+            {
+                cart.CartLines.Add(new CartLine() { Product = product, Quantity = 1 });
+            }
+        }
+
+        public void RemoveFromCart(Cart cart, int productId)
+        {
+            cart.CartLines.Remove(cart.CartLines.FirstOrDefault(line => line.Product.ProductId == productId));
+        }
+
+        public List<CartLine> List(Cart cart)
+        {
+            return cart.CartLines;
+        }
+    }
+}
---
+++ NorthwindLevelTwo/Entities/DomainModels/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Entities.Abstract;
+
+namespace Entities.DomainModels
+{
+    public class Cart : IDomainModel
+    {
+        public Cart()
+        {
+            CartLines = new List<CartLine>();
+        }
+        public List<CartLine> CartLines { get; set; }
+
+        public decimal Total
+        {
+            get { return CartLines.Sum(line => line.Quantity * line.Product.UnitPrice); }
+        }
+    }
+}
