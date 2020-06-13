d20bd3315ea649c46ae0ff4d04675638adc48bef
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-06-13 11:21:45 +0300

    Creating and implementing Work Repository

---
+++ Example-2/ToDo/Arif.ToDo.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfWorkRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.ToDo.DataAccess.Interfaces;
+using Arif.ToDo.Entities.Concrete;
+
+namespace Arif.ToDo.DataAccess.Concrete.EntityFrameworkCore.Repositories
+{
+    public class EfWorkRepository : IWorkDal
+    {
+        public void SaveWork(Work work)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteWork(Work work)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateWork(Work work)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Work GetWorkById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Work> GetAllWorks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Example-2/ToDo/Arif.ToDo.DataAccess/Interfaces/IWorkDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.ToDo.Entities.Concrete;
+
+namespace Arif.ToDo.DataAccess.Interfaces
+{
+    public interface IWorkDal
+    {
+        void SaveWork(Work work);
+        void DeleteWork(Work work);
+        void UpdateWork(Work work);
+        Work GetWorkById(int id);
+        List<Work> GetAllWorks();
+    }
+}
