cf5317927237e7192679c110a9f56ca213f8b6c9
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-06-14 10:20:18 +0300

    Adding EF core packages and creating TodoContext

---
+++ Example-2/ToDo/Arif.ToDo.DataAccess/Concrete/EntityFrameworkCore/Contexts/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.ToDo.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace Arif.ToDo.DataAccess.Concrete.EntityFrameworkCore.Contexts
+{
+    public class TodoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\ProjectsV13;database=ToDo;integrated security=true;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Work> Works { get; set; }
+    }
+}
