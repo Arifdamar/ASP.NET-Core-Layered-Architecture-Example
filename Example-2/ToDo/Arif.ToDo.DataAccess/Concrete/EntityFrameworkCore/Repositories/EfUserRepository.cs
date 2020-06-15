3b514db8379ae5ee614a214a837f4264d95c7e26
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-06-15 13:49:14 +0300

    Declaring UserRepository operations

--- Example-2/ToDo/Arif.ToDo.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfUserRepository.cs
+++ Example-2/ToDo/Arif.ToDo.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfUserRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Arif.ToDo.DataAccess.Concrete.EntityFrameworkCore.Contexts;
 using Arif.ToDo.DataAccess.Interfaces;
 using Arif.ToDo.Entities.Concrete;
 
@@ -9,27 +11,35 @@ namespace Arif.ToDo.DataAccess.Concrete.EntityFrameworkCore.Repositories
     {
         public void Save(User table)
         {
-            throw new NotImplementedException();
+            using var context = new TodoContext();
+            context.Users.Add(table);
+            context.SaveChanges();
         }
 
         public void Delete(User table)
         {
-            throw new NotImplementedException();
+            using var context = new TodoContext();
+            context.Users.Remove(table);
+            context.SaveChanges();
         }
 
         public void Update(User table)
         {
-            throw new NotImplementedException();
+            using var context = new TodoContext();
+            context.Users.Update(table);
+            context.SaveChanges();
         }
 
         public User GetById(int id)
         {
-            throw new NotImplementedException();
+            using var context = new TodoContext();
+            return context.Users.Find(id);
         }
 
         public List<User> GetAll()
         {
-            throw new NotImplementedException();
+            using var context = new TodoContext();
+            return context.Users.ToList();
         }
     }
 }
