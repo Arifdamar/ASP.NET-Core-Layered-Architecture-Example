522dfa749285eb63fae7984434e345168101c2a8
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-06-15 16:03:59 +0300

    Adding Business layer's interfaces

--- Example-2/ToDo/Arif.ToDo.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Arif.ToDo.Business
-{
-    public class Class1
-    {
-    }
-}
---
+++ Example-2/ToDo/Arif.ToDo.Business/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.ToDo.Entities.Concrete;
+
+namespace Arif.ToDo.Business.Interfaces
+{
+    public interface IUserService : IGenericService<User>
+    {
+
+    }
+}
