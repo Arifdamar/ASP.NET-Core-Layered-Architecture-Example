07b7da73bb28fa606d576ed89453280b404366d4
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-08-04 16:59:12 +0300

    listing tasks

--- Example-2/ToDo/Arif.ToDo.Web/Areas/Admin/Controllers/TaskController.cs
+++ Example-2/ToDo/Arif.ToDo.Web/Areas/Admin/Controllers/TaskController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Arif.ToDo.Business.Interfaces;
+using Arif.ToDo.Web.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Arif.ToDo.Web.Areas.Admin.Controllers
@@ -9,11 +11,34 @@ namespace Arif.ToDo.Web.Areas.Admin.Controllers
     [Area("Admin")]
     public class TaskController : Controller
     {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
         public IActionResult Index()
         {
             TempData["Active"] = "task";
+            var tasks = _taskService.GetAll();
+            List<TaskListViewModel> viewModel = new List<TaskListViewModel>();
 
-            return View();
+            tasks.ForEach(task =>
+            {
+                viewModel.Add(new TaskListViewModel()
+                {
+                    Id = task.Id,
+                    Name = task.Name,
+                    Description = task.Description,
+                    CratedDate = task.CratedDate,
+                    Status = task.Status,
+                    UrgencyId = task.UrgencyId,
+                    Urgency = task.Urgency
+                });
+            });
+
+            return View(viewModel);
         }
     }
 }
