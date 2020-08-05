beba12cc8fe284de70d550bbdaa8a0720fd26a97
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-08-06 00:46:19 +0300

    user registration

--- Example-2/ToDo/Arif.ToDo.Web/Controllers/HomeController.cs
+++ Example-2/ToDo/Arif.ToDo.Web/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Arif.ToDo.Business.Interfaces;
+using Arif.ToDo.Entities.Concrete;
 using Arif.ToDo.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Arif.ToDo.Web.Controllers
@@ -11,10 +13,12 @@ namespace Arif.ToDo.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ITaskService _taskService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ITaskService taskService)
+        public HomeController(ITaskService taskService, UserManager<AppUser> userManager)
         {
             _taskService = taskService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -25,6 +29,45 @@ namespace Arif.ToDo.Web.Controllers
         public IActionResult Register()
         {
             return View(new AppUserAddViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(AppUserAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    Name = model.Name,
+                    SurName = model.SurName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var addRoleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+                    if (addRoleResult.Succeeded)
+                    {
+                        return RedirectToAction("SignIn");
+                    }
+
+                    foreach (var error in addRoleResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+
+            return View(model);
         }
     }
 }
