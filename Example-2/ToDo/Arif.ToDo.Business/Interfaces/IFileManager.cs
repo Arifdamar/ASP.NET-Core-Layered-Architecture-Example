16f798cd24de229c2f5dc61c8f29526dfdff5bcd
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-08-13 14:28:50 +0300

    exporting operations (pdf and excel)

---
+++ Example-2/ToDo/Arif.ToDo.Business/Interfaces/IFileManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arif.ToDo.Business.Interfaces
+{
+    public interface IFileManager
+    {
+        /// <summary>
+        /// Geriye üretmiş ve upload etmiş olduğu pdf dosyasının virtual pathini döner.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        string ExportPdf<T>(List<T> list) where T : class, new();
+        /// <summary>
+        /// Geriye excel verisini byte dizisi olarak döner.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        byte[] ExportExcel<T>(List<T> list) where T : class, new();
+    }
+}
