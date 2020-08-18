78625a04aabd0e243e88f928a97833129386492f
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-08-18 14:15:55 +0300

    add notifications page

---
+++ Example-2/ToDo/Arif.ToDo.Web/Areas/Admin/Controllers/NotificationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Arif.ToDo.Business.Interfaces;
+using Arif.ToDo.Entities.Concrete;
+using Arif.ToDo.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arif.ToDo.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class NotificationController : Controller
+    {
+        private readonly INotificationService _notificationService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public NotificationController(INotificationService notificationService, UserManager<AppUser> userManager)
+        {
+            _notificationService = notificationService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var notifications = _notificationService.GetUnreadNotificationsById(user.Id);
+            List<NotificationListViewModel> model = new List<NotificationListViewModel>();
+
+            notifications.ForEach(notification =>
+            {
+                model.Add(new NotificationListViewModel()
+                {
+                    Id = notification.Id,
+                    Description = notification.Description
+                });
+            });
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            var notification = _notificationService.GetById(id);
+            notification.Status = true;
+            _notificationService.Update(notification);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
