47b5cafeeece7aac3c9bdbf3aa64cae0563a2870
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-08-20 16:38:11 +0300

    add fluent validation

---
+++ Example-2/ToDo/Arif.ToDo.Business/ValidationRules/FluentValidation/UrgencyAddValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.ToDo.DTO.DTOs.UrgencyDTOs;
+using FluentValidation;
+
+namespace Arif.ToDo.Business.ValidationRules.FluentValidation
+{
+    public class UrgencyAddValidator : AbstractValidator<UrgencyAddDto>
+    {
+        public UrgencyAddValidator()
+        {
+            RuleFor(I => I.Description).NotNull().WithMessage("Tanım Alanı Boş Geçilemez");
+        }
+    }
+}
---
+++ Example-2/ToDo/Arif.ToDo.Business/ValidationRules/FluentValidation/UrgencyUpdateValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.ToDo.DTO.DTOs.UrgencyDTOs;
+using FluentValidation;
+
+namespace Arif.ToDo.Business.ValidationRules.FluentValidation
+{
+    public class UrgencyUpdateValidator : AbstractValidator<UrgencyUpdateDto>
+    {
+        public UrgencyUpdateValidator()
+        {
+            RuleFor(I => I.Description).NotNull().WithMessage("Tanım Alanı Boş Geçilemez");
+        }
+    }
+}
